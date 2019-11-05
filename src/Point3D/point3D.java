5137f057eaed93a16c827b356d8c4902816c5c23
Shourya Shukla <43680618+g0pn1k@users.noreply.github.com>
2019-11-06 00:34:10 +0530

    adds the z-coordinate to all functions and constructors

--- src/Point3D/point3D.java
+++ src/Point3D/point3D.java
@@ -32,14 +32,14 @@ package Point3D;
  * 
  * @since Android 1.0
  */
-public abstract class Point2D implements Cloneable {
+public abstract class Point3D implements Cloneable {
     /**
      * The Class Float is the subclass of Point2D that has all of its data
      * values stored with float-level precision.
      * 
      * @since Android 1.0
      */
-    public static class Float extends Point2D {
+    public static class Float extends Point3D{
         /**
          * The x coordinate.
          */
@@ -48,6 +48,10 @@ public abstract class Point2D implements Cloneable {
          * The y coordinate.
          */
         public float y;
+        /**
+         * The z coordinate.
+         */
+        public float z;
         /**
          * Instantiates a new float-valued Point2D with its data set to zero.
          */
@@ -61,10 +65,13 @@ public abstract class Point2D implements Cloneable {
          *            the x coordinate.
          * @param y
          *            the y coordinate.
+         * @param z
+         *            the z coordinate.
          */
-        public Float(float x, float y) {
+        public Float(float x, float y, float z) {
             this.x = x;
             this.y = y;
+            this.z = z;
         }
         @Override
         public double getX() {
@@ -74,6 +81,10 @@ public abstract class Point2D implements Cloneable {
         public double getY() {
             return y;
         }
+        @Override
+        public double getZ() {     //TODO check the func getZ()
+            return z;
+        }
         /**
          * Sets the point's coordinates.
          * 
@@ -81,19 +92,23 @@ public abstract class Point2D implements Cloneable {
          *            the x coordinate.
          * @param y
          *            the y coordinate.
+         * @param z
+         *            the z coordinate.
          */
-        public void setLocation(float x, float y) {
+        public void setLocation(float x, float y, float z) {
             this.x = x;
             this.y = y;
+            this.z = z;
         }
         @Override
-        public void setLocation(double x, double y) {
+        public void setLocation(double x, double y, double z) {
             this.x = (float)x;
             this.y = (float)y;
+            this.z = (float)z;
         }
         @Override
         public String toString() {
-            return getClass().getName() + "[x=" + x + ",y=" + y + "]"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            return getClass().getName() + "[x=" + x + ",y=" + y + "z=" + z + "]"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         }
     }
     /**
@@ -102,7 +117,7 @@ public abstract class Point2D implements Cloneable {
      * 
      * @since Android 1.0
      */
-    public static class Double extends Point2D {
+    public static class Double extends Point3D {
         /**
          * The x coordinate.
          */
@@ -111,6 +126,10 @@ public abstract class Point2D implements Cloneable {
          * The y coordinate.
          */
         public double y;
+        /**
+         * The z coordinate.
+         */
+        public double z;
         /**
          * Instantiates a new double-valued Point2D with its data set to zero.
          */
@@ -124,10 +143,13 @@ public abstract class Point2D implements Cloneable {
          *            the x coordinate.
          * @param y
          *            the y coordinate.
+         * @param z
+         *            the z coordinate.
          */
-        public Double(double x, double y) {
+        public Double(double x, double y, double z) {
             this.x = x;
             this.y = y;
+            this.z = z;
         }
         @Override
         public double getX() {
@@ -138,19 +160,25 @@ public abstract class Point2D implements Cloneable {
             return y;
         }
         @Override
-        public void setLocation(double x, double y) {
+        public double getZ() {
+            return z;
+        }
+        @Override
+        public void setLocation(double x, double y, double z) {
             this.x = x;
             this.y = y;
+            this.z = z;
         }
         @Override
         public String toString() {
-            return getClass().getName() + "[x=" + x + ",y=" + y + "]"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            return getClass().getName() + "[x=" + x + ",y=" + y + "z=" + z + "]"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         }
-    }
+    }  // END OF CLASS DOUBLE
+   
     /**
      * Instantiates a new Point2D.
      */
-    protected Point2D() {
+    protected Point3D() {
     }
     /**
      * Gets the x coordinate.
@@ -165,22 +193,30 @@ public abstract class Point2D implements Cloneable {
      */
     public abstract double getY();
     /**
+    * Gets the z coordinate.
+    * 
+    * @return the z coordinate.
+    */
+   public abstract double getZ();
+    /**
      * Sets the point's coordinates.
      * 
      * @param x
      *            the x coordinate.
      * @param y
      *            the y coordinate.
+     * @param z
+     *            the z coordinate.
      */
-    public abstract void setLocation(double x, double y);
+    public abstract void setLocation(double x, double y, double z);
     /**
      * Sets the point's coordinates by copying them from another point.
      * 
      * @param p
      *            the point to copy the data from.
      */
-    public void setLocation(Point2D p) {
-        setLocation(p.getX(), p.getY());
+    public void setLocation(Point3D p) {
+        setLocation(p.getX(), p.getY(), p.getZ());
     }
     /**
      * Finds the square of the distance between the two specified points.
@@ -189,16 +225,21 @@ public abstract class Point2D implements Cloneable {
      *            the x coordinate of the first point.
      * @param y1
      *            the y coordinate of the first point.
+     * @param z1
+     *            the z coordinate of the first point.           
      * @param x2
      *            the x coordinate of the second point.
      * @param y2
      *            the y coordinate of the second point.
+     * @param z2
+     *            the z coordinate of the second point.           
      * @return the square of the distance between the two specified points.
      */
-    public static double distanceSq(double x1, double y1, double x2, double y2) {
+    public static double distanceSq(double x1, double y1, double z1, double x2, double y2, double z2) {
         x2 -= x1;
         y2 -= y1;
-        return x2 * x2 + y2 * y2;
+        z2 -= z1;
+        return x2 * x2 + y2 * y2 + z2 * z2;
     }
     /**
      * Finds the square of the distance between this point and the specified
@@ -208,11 +249,13 @@ public abstract class Point2D implements Cloneable {
      *            the x coordinate of the point.
      * @param py
      *            the y coordinate of the point.
+     * @param pz
+     *            the z coordinate of the point.
      * @return the square of the distance between this point and the specified
      *         point.
      */
-    public double distanceSq(double px, double py) {
-        return Point2D.distanceSq(getX(), getY(), px, py);
+    public double distanceSq(double px, double py, double pz) {
+        return Point3D.distanceSq(getX(), getY(), getZ(), px, py, pz);
     }
     /**
      * Finds the square of the distance between this point and the specified
@@ -223,8 +266,8 @@ public abstract class Point2D implements Cloneable {
      * @return the square of the distance between this point and the specified
      *         point.
      */
-    public double distanceSq(Point2D p) {
-        return Point2D.distanceSq(getX(), getY(), p.getX(), p.getY());
+    public double distanceSq(Point3D p) {
+        return Point3D.distanceSq(getX(), getY(), getZ(), p.getX(), p.getY(), p.getZ());
     }
     /**
      * Finds the distance between the two specified points.
@@ -233,14 +276,18 @@ public abstract class Point2D implements Cloneable {
      *            the x coordinate of the first point.
      * @param y1
      *            the y coordinate of the first point.
+     * @param z1
+     *            the z coordinate of the first point.           
      * @param x2
      *            the x coordinate of the second point.
      * @param y2
      *            the y coordinate of the second point.
+     * @param z2
+     *            the z coordinate of the second point.           
      * @return the distance between the two specified points.
      */
-    public static double distance(double x1, double y1, double x2, double y2) {
-        return Math.sqrt(distanceSq(x1, y1, x2, y2));
+    public static double distance(double x1, double y1,double z1, double x2, double y2, double z2) {
+        return Math.sqrt(distanceSq(x1, y1, z1, x2, y2, z2));
     }
     /**
      * Finds the distance between this point and the specified point.
@@ -249,10 +296,12 @@ public abstract class Point2D implements Cloneable {
      *            the x coordinate of the point.
      * @param py
      *            the y coordinate of the point.
+     * @param pz
+     *            the z coordinate of the point.            
      * @return the distance between this point and the specified point.
      */
-    public double distance(double px, double py) {
-        return Math.sqrt(distanceSq(px, py));
+    public double distance(double px, double py, double pz) {
+        return Math.sqrt(distanceSq(px, py, pz));
     }
     /**
      * Finds the distance between this point and the specified point.
@@ -261,7 +310,7 @@ public abstract class Point2D implements Cloneable {
      *            the other point.
      * @return the distance between this point and the specified point.
      */
-    public double distance(Point2D p) {
+    public double distance(Point3D p) {
         return Math.sqrt(distanceSq(p));
     }
     @Override
@@ -273,10 +322,11 @@ public abstract class Point2D implements Cloneable {
         }
     }
     @Override
-    public int hashCode() {
-        HashCode hash = new HashCode();
+    public int hashCode() {                //TODO IMPLEMENT HASHCODE
+        HashCode hash = new HashCode(); 
         hash.append(getX());
         hash.append(getY());
+        hash.append(getZ());
         return hash.hashCode();
     }
     @Override
@@ -284,9 +334,9 @@ public abstract class Point2D implements Cloneable {
         if (obj == this) {
             return true;
         }
-        if (obj instanceof Point2D) {
-            Point2D p = (Point2D)obj;
-            return getX() == p.getX() && getY() == p.getY();
+        if (obj instanceof Point3D) {
+            Point3D p = (Point3D)obj;
+            return getX() == p.getX() && getY() == p.getY() && getZ()==p.getZ();
         }
         return false;
     }
